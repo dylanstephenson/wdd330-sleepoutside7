6a1b18d2df0da7d5b67f220f818aca5986866f02
Caitlin Evans <caitmsevans@gmail.com>
2025-03-11 18:47:43 -0600

    Delete item from cart X in cart page

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,6 +7,13 @@ function renderCartContents() {
   if (cartItems !== null && cartItems !== undefined) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    //eventlistener for the close btn 
+    const deleteButtons = document.querySelectorAll('.close-btn');
+    deleteButtons.forEach((button) => {
+      button.addEventListener('click', function() {
+        deleteItem(button.getAttribute('data-id'));
+      });
+    });
   }
 }
 
@@ -14,6 +21,7 @@ function renderCartContents() {
 //template literal
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <button class='close-btn' data-id='${item.Id}'>X</button>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -32,3 +40,30 @@ function cartItemTemplate(item) {
 }
 //populate the cart info on cart page
 renderCartContents();
+//gets the Cart Count for the backpack superscript
+renderCartCount();
+
+function deleteItem(id) {
+  var cartItems = getLocalStorage('so-cart');
+
+  if (cartItems) {
+    //find the index with the first id
+    const itemIndex = cartItems.findIndex(item => item.Id === id);
+
+    // if you find the item, delete it from the array
+    if (itemIndex !== -1) {
+      cartItems.splice(itemIndex, 1);
+
+      //delete the previous so-cart in localStorage 
+      localStorage.clear();
+
+      //save the new object cart in localStorage and make it json object.
+      localStorage.setItem('so-cart', JSON.stringify(cartItems))
+
+      //populate the cart info on cart page again
+      renderCartContents();
+      //gets the Cart Count for the backpack superscript
+      renderCartCount();
+    }
+  }
+}
