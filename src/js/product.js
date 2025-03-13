74602f0792b43e598c143874d840c81dbda3d6a9
Caitlin Evans <caitmsevans@gmail.com>
2025-03-13 09:57:29 -0600

    Week 3 header footer dynamically rendered and cart changes from week 2 changed to fit new header footer

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,12 @@
-//js for the product page
-//get the productid from dynamic searchquery (url) using getParams in utils.js
-//create product Data class from json using ProductData in ProductData.js
-//create a ProductDetails class based on the id and data class.
-//initialize productDetails which builds the product page detils
-
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { getParams, renderCartCount } from "./utils.mjs";
+import { loadHeaderFooter, getParams } from "./utils.mjs";
 
 const productId = getParams("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-//gets cart count for products page
-renderCartCount();
+
+//wk3 dynamic header footer 
+loadHeaderFooter();
