43bc725f1be5056d58a73f9257341d4869e80bd2
EM-OBI <gbassey1@byupathway.edu>
2025-03-14 20:33:13 +0100

    Fixed inconsistencies with quotation marks by making all strings double quotes and ran npm format

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 //js for the main html page
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
-import { renderCartCount } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { renderCartCount } from "./utils.mjs";
 
 //create ProductData object
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 //create listing object
-const listing = new ProductList('tents', dataSource, 'ul');
+const listing = new ProductList("tents", dataSource, "ul");
 //initialize listing object
 listing.init();
 //grabs cart count on main html pag
--- src/js/product.js
+++ src/js/product.js
@@ -4,12 +4,12 @@
 //create a ProductDetails class based on the id and data class.
 //initialize productDetails which builds the product page detils
 
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
-import { getParams, renderCartCount  } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParams, renderCartCount } from "./utils.mjs";
 
-const productId = getParams('product');
-const dataSource = new ProductData('tents');
+const productId = getParams("product");
+const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
